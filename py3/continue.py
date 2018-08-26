88c82001830daef9646a3b62f1bf93fe1df1ff51
liucz <liuchangzheng25@163.com>
2018-08-26 20:59:53 +0800

    shiyan5 xunhuan wancheng

---
+++ py3/continue.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+while True:
+    n= int (input("Please enter an Interger:"))
+    if n< 0:
+        print ("error input")
+        continue
+    elif n==0:
+        break
+    print ("Square is ",n**2)
+print("Goodbye")
