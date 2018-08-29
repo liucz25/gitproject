a0d3060bf21f5013436a11e909848e54f2bc772e
liucz <liuchangzheng25@163.com>
2018-08-29 23:19:01 +0800

    module 1

---
+++ py3/code12/mymodule/bars.py
@@ -0,0 +1,12 @@
+"""
+bar Module
+====================
+fei ge xian shili 
+
+"""
+def starbar(num):
+    print ("*"*num)
+def hashbar(num):
+    print ("#"*num)
+def simplebar(num):
+    print ("-"*num)
