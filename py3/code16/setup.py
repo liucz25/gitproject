64b828d0a121bf807d72c315401960ae6733e2a1
liucz <liuchangzheng25@163.com>
2018-09-01 21:22:06 +0800

    setup

---
+++ py3/code16/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from setuptools import find_packages,setup
+
+setup(name = 'factorial',
+    version = '0,1',
+    description = 'Factorial modle.',
+    long_description = "A test module for our book.",
+    platforms = ["Linux"],
+    author = "liuchangzheng",
+    license = "MIT",
+    packages=find_packages()
+    )
