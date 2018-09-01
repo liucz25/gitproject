aca19e60d6ecbb6866731a02e122dd0784630e54
liucz <liuchangzheng25@163.com>
2018-09-01 22:10:27 +0800

    flask begin

---
+++ py3/code18/hello_flask/hello_flask.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import flask
+
+
+# Create the application.
+APP = flask.Flask(__name__)
+
+
+@APP.route('/')
+def index():
+    """ ???? '/' ??? index ??
+    """
+    return flask.render_template('index.html')
+
+
+@APP.route('/hello/<name>/')
+def hello(name):
+    """ Displays the page greats who ever comes to visit it.
+    """
+    return flask.render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    APP.debug=True
+    APP.run()
